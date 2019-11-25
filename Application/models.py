25cfc83fd4f2e4a693b409d007ec778ae4ffad1b
Gaetan Jonathan <gaetan.jonathan.bakary@esti.mg>
2019-11-25 14:43:05 +0300

    Page fonctionnel Creation des models Fichiers static Creation de compte

--- Application/models.py
+++ Application/models.py
@@ -1,3 +1,22 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Game(models.Model):
+    titre = models.CharField(max_length=50)
+    couverture = models.URLField()
+    categorie = models.CharField(max_length=50)
+    gameplay = models.URLField()
+    prix = models.IntegerField()
+
+
+    def __str__(self):
+        return self.titre + ': ' + self.categorie
+
+
+class Phone(models.Model):
+    username = models.ForeignKey(User, on_delete=models.CASCADE)
+    phone = models.CharField(max_length=15)
+
+
+    def __str__(self):
+        return f"{self.username : self.phone}"
--- Application/views.py
+++ Application/views.py
@@ -1,3 +1,29 @@
 from django.shortcuts import render
-
+from .models import Game 
+from django.contrib.auth.models import User
+from django.http import JsonResponse, HttpResponse, Http404
 # Create your views here.
+
+def home(request):
+    return render(request, 'Application/index.html', locals())
+
+
+def create(request):
+    if request.method == "POST":
+        nom = request.POST.get('nom').capitalize()
+        prenom = request.POST.get('prenom').capitalize()
+        mail = request.POST.get('mail').lower()
+        phone = request.POST.get('phone')
+        password = request.POST.get('password')
+        username = f"{nom}.{prenom}"
+
+        if len(User.objects.filter(email=mail) == 1):
+            return HttpResponse("Vous avez déjà un compte")
+
+        usr = User(username=username, password=password, email=mail, first_name=nom, last_name=prenom)
+
+        usr.save()
+
+        return HttpResponse(1)
+
+    raise Http404("Chemin Non Trouvé")
